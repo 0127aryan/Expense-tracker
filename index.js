0c989ca50751b60a6bce96eab987263a382fe513
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-08-26 18:20:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const expenseform = document.getElementById('expenseform')
+const expenseList = document.getElementById('expenseList')
+
+expenseform.addEventListener('submit', function(event){
+    event.preventDefault()
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
+    const amount = document.getElementById('amount').value;
+
+    if(document && category && !isNaN(amount)){
+        const newRow = document.createElement('tr')
+
+        newRow.innerHTML = `<td>${description}</td>
+        <td>${category}</td>
+        <td>${amount}</td>`;
+
+        expenseList.appendChild(newRow)
+
+        document.getElementById('description').value = ''
+        document.getElementById('category').value = ''
+        document.getElementById('amount').value = ''
+    } else{
+        alert('please fill out all fields with valid data')
+    }
+    
+})
